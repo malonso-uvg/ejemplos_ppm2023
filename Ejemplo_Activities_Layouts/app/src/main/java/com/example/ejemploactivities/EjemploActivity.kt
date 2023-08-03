883881e5e88aa8d9de0eafc13061d9a9579b0b4c
Moises Alonso <maalonso@uvg.edu.gt>
2023-08-03 10:15:11 -0600

    Agregar ejemplo de como crear elementos graficos desde codigo

---
+++ Ejemplo_Activities_Layouts/app/src/main/java/com/example/ejemploactivities/EjemploActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ejemploactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EjemploActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejemplo)
+    }
+}
--- Ejemplo_Activities_Layouts/app/src/main/java/com/example/ejemploactivities/LinearLayoutActivity.kt
+++ Ejemplo_Activities_Layouts/app/src/main/java/com/example/ejemploactivities/LinearLayoutActivity.kt
@@ -2,10 +2,34 @@ package com.example.ejemploactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class LinearLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_linear_layout)
+
+        var cantidad_botones: Int = 0
+
+        while (cantidad_botones < 5){
+
+            //Creo el objeto de tipo Boton
+            val miBoton = Button(this)
+            miBoton.text = "Buton Generado " + cantidad_botones
+            miBoton.setBackgroundColor(getResources().getColor(R.color.azul))
+            miBoton.setOnClickListener{
+                val toast = Toast.makeText(this, "Boton presionado", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+
+            //Busco el Layout y agrego el boton creado
+            val miLayout = findViewById<LinearLayout>(R.id.linearLayout1)
+            miLayout.addView(miBoton)
+
+            cantidad_botones++
+        }
+
     }
 }
